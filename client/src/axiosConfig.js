bf60bf59acbeb0e8e252dad0dc7ea31b9d74cd18
SOLOMON ADUGNA <tegiesolomonadugna5@gmail.com>
2025-10-25 21:48:29 -0700

    Base URL update

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://q-a-by-sol.onrender.com/api/users",  // 👈 correct prefix
+ baseURL: 'https://q-a-by-sol.onrender.com/api'
 });
