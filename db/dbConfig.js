ab37a36eaf38b5404d4cffd2e5a41ae963b7c5f5
SOLOMON ADUGNA <tegiesolomonadugna5@gmail.com>
2025-10-26 03:04:11 -0700

    Db Name update

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -14,6 +14,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
+   database: process.env.DB_NAME,
   // database: process.env.DB_NAME || 'evangadi-db',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
